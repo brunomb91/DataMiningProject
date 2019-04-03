325b5f6053b38a858229931aa145beeaeb2593da
unknown <bruno.mb.91@gmail.com>
2019-04-03 02:38:11 -0300

    Mudanças e novos arquivos

--- data-analysis.R
+++ data-analysis.R
@@ -38,8 +38,23 @@ plot(data$Temp)
 data$Data<- ymd_hms(data$Data)
 data$Data <- as.POSIXct(data$Data,tz="UTC")
 
+# Plotando gráficos da base de dados
 myplot1 <- ggplot(data,aes(Data))+geom_line(color="Red",aes(y=Temp))+ylab("Temperature")+xlab("Time")+
-           scale_x_datetime(breaks=date_breaks("1440 min"),labels=date_format("%H:%M"),
-           limits=as.POSIXct(c("2011-01-01 8:00","2011-12-31 9:00"),tz="GMT"))+
-           theme(axis.text.x=element_text(angle=90,hjust=1))
+                  scale_x_datetime(breaks=date_breaks("1440 min"),labels=date_format("%H:%M"),
+                  limits=as.POSIXct(c("2012-01-01 8:00","2012-12-31 9:00"),tz="GMT"))+
+                  theme(axis.text.x=element_text(angle=90,hjust=1))
 print(myplot1, vp = viewport(layout.pos.row = 1, layout.pos.col = 1))
+
+myplot2 <- ggplot(data,aes(Data))+geom_line(color="Blue",aes(y=SO2))+ylab("SO2")+xlab("Time")+
+                  scale_x_datetime(breaks=date_breaks("1440 min"),labels=date_format("%H:%M"),
+                  limits=as.POSIXct(c("2012-01-01 8:00","2012-12-31 9:00"),tz="GMT"))+
+                  theme(axis.text.x=element_text(angle=90,hjust=1))
+print(myplot2, vp = viewport(layout.pos.row = 1, layout.pos.col = 1))
+
+myplot1 <- ggplot_gtable(ggplot_build(myplot1))
+myplot2 <- ggplot_gtable(ggplot_build(myplot2))
+maxWidth = unit.pmax(myplot1$widths[2:3],myplot2$widths[2:3])
+
+myplot1$widths[2:3] <- maxWidth
+grid.arrange(myplot1, myplot2, ncol=1)
+
