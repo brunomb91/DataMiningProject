4cfd6a5cc11eeed0b408d9881d27af3f74460090
unknown <bruno.mb.91@gmail.com>
2019-04-02 23:14:52 -0300

    Fazendo plots

--- data-analysis.R
+++ data-analysis.R
@@ -1,4 +1,14 @@
+# Instalação e chamada dos pacotes necessários
 install.packages('ggplot2')
+install.packages('lubridate')
+install.packages('gridExtra')
+install.packages('scales')
+install.packages('rattle')
+install.packages('randomForest')
+install.packages('caret')
+install.packages('Hmisc')
+install.packages('e1071')
+install.packages('corrplot')
 
 library(lubridate)
 library(ggplot2)
@@ -12,18 +22,23 @@ library(Hmisc)
 library(e1071)
 library(corrplot)
 
-summary(data1)
+# Importação da base de dados
+data = read.csv('data/Dados_horários_do_monitoramento_da_qualidade_do_ar__MonitorAr.csv')
+
+summary(data)
 
 # data1 <-data1[!is.na(data1$light),]
 
-data1[is.na(data1)] <- 0
+# Retirando os termos NA da base de dados
+data[is.na(data)] <- 0
 
-plot(data1$Temp)
+plot(data$Temp)
 
-data1$Data<- ymd_hms(data1$Data)
-data1$Data <- as.POSIXct(data1$Data,tz="UTC")
+# Alterando formato de data para o formato UTC
+data$Data<- ymd_hms(data$Data)
+data$Data <- as.POSIXct(data$Data,tz="UTC")
 
-myplot1 <- ggplot(data1,aes(Data))+geom_line(color="Red",aes(y=Temp))+ylab("Temperature")+xlab("Time")+
+myplot1 <- ggplot(data,aes(Data))+geom_line(color="Red",aes(y=Temp))+ylab("Temperature")+xlab("Time")+
            scale_x_datetime(breaks=date_breaks("1440 min"),labels=date_format("%H:%M"),
            limits=as.POSIXct(c("2011-01-01 8:00","2011-12-31 9:00"),tz="GMT"))+
            theme(axis.text.x=element_text(angle=90,hjust=1))
